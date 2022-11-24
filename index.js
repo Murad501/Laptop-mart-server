cb431922fef1f63a7934db889156d519e7e06726
Murad501 <mm845635@gmail.com>
2022-11-24 20:35:22 +0600

    user add api created

--- index.js
+++ index.js
@@ -12,7 +12,27 @@ app.get('/', (req, res)=>{
 })
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@user1.istzhai.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const run = async() => {
+    try{
+        const userCollection = client.db('LaptopMart').collection('users')
+
+        //users
+        app.post('/users', async(req, res)=>{
+            console.log(req.body)
+            const user = req.body
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+    }
+    catch{
+
+    }
+}
+run().catch(err => console.log(err))
 
 app.listen(port, ()=>{
     console.log('server running on port', port)
